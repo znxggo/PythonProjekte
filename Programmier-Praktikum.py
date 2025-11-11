35f1fceba8792615300cc1e2028431f53bd2ec2a
Nikolas Tobias Auer <niko.auer49@gmail.com>
2025-11-11 15:57:37 +0100

    Commit 3

--- Programmier-Praktikum.py
+++ Programmier-Praktikum.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Liste erstellen mit 10 Gliedern von 0 bis 10
+#Liste erstellen mit 10 Gliedern von 0 bis 9
 #x= np.array([0,1,2,3,4,5,6,7,8,9])
 #print(x)
 
