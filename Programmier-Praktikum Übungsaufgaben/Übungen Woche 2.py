3987b6de86752e97c3c9f9f11a2856712de7a295
znxggo <niko.auer49@gmail.com>
2025-11-18 12:13:01 +0100

    Commit

--- Programmier-Praktikum Übungsaufgaben/Übungen Woche 2.py
+++ Programmier-Praktikum Übungsaufgaben/Übungen Woche 2.py
@@ -1,6 +1,34 @@
 #Aufgaben Woche 2
 
+#Aufgabe 1
+
 def to_binary(n: int) -> str:
     return bin(n)
 
 print(to_binary(42))  #erwartet:0b101010
+
+#Aufgabe 2
+
+print(oct(255))
+print(hex(255))
+
+#Aufgabe 3
+
+global x
+x= "global"
+
+def func():
+    x = "lokal"
+    print("In func:", x)
+
+func()
+print("Außerhalb:", x)
+
+    #Fragestellung: Was gibt das Programm aus? Erkläre kurz warum.
+
+    #Antwort:
+    #Die Funktion func() gibt [In func: lokal] aus
+    #print("Außerhalb:", x) gibt [Außerhalb: global] aus
+
+#Aufgabe 3
+
