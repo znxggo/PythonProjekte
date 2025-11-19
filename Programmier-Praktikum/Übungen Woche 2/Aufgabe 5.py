08e97d9d24f32eef5e36b6312a530c4daff602d8
Nikolas Tobias Auer <niko.auer49@gmail.com>
2025-11-19 16:23:07 +0100

    Commit & Push

--- Programmier-Praktikum/Übungen Woche 2/Aufgabe 5.py
+++ Programmier-Praktikum/Übungen Woche 2/Aufgabe 5.py
@@ -1,3 +1,11 @@
 #Aufgabe 5
 
-#Mach ich zuhause
+#Implementiere Liste zahlen mit [1,2,3]
+zahlen = [1,2,3]
+#Erweitere Liste zahlen um [4,5]
+zahlen += [4, 5]
+#Gib aus, ob die Zahl 3 in der Liste zahlen enthalten ist
+print(3 in zahlen)
+#Verdoppelung und Ausgabe der Liste zahlen
+zahlen = zahlen * 2
+print(zahlen)
