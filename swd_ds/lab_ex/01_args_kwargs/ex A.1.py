ba1ff369fabade92501cc31d93db884abd398eb8
znxggo <niko.auer49@gmail.com>
2026-03-20 21:40:39 +0100

    Commit & Push

---
+++ swd_ds/lab_ex/01_args_kwargs/ex A.1.py
@@ -0,0 +1,19 @@
+def total(*numbers):
+   return(sum(numbers))
+
+def average(*numbers):
+    return(average(numbers))
+
+def clamp_all(low, high, *numbers):
+    return()
+
+
+print(total(1, 2, 3, 4))
+
+print(total())
+
+print(average(10, 20, 30))
+
+print(average())
+
+print(clamp_all(0, 100, -5, 50, 120, 75))
